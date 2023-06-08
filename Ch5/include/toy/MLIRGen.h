8b34125ce87e95b75bce85256cdc26f523b1e168
xZacky <1462755154@qq.com>
2023-06-08 21:17:29 +0800

    Update to Ch5

---
+++ Ch5/include/toy/MLIRGen.h
@@ -0,0 +1,32 @@
+//===-------------- MLIRGen.h - MLIR Generation from a Toy AST --------------===//
+//
+// This file declares a simple interface to perform IR generation targeting
+// MLIR from a Module AST for the Toy language.
+//
+//===------------------------------------------------------------------------===//
+
+#ifndef TOY_MLIRGEN_H
+#define TOY_MLIRGEN_H
+
+#include <memory>
+
+namespace mlir {
+
+class MLIRContext;
+template <typename OpTy>
+class OwningOpRef;
+class ModuleOp;
+
+} // namespace mlir
+
+namespace toy {
+
+class ModuleAST;
+
+// Emit IR for the given Toy moduleAST, returns a newly created MLIR module
+// or nullptr on failure.
+mlir::OwningOpRef<mlir::ModuleOp> mlirGen(mlir::MLIRContext &context,
+                                         ModuleAST &moduleAST);
+} // namespace toy
+
+#endif // TOY_MLIR_H
